65f19aee80288b348f565e76db6fca36bda4b20c
Bajlur Rahaman <bajlur@stksoftware.com>
2021-02-05 22:11:07 +0530

    Some Page add

--- js/custom.js
+++ js/custom.js
@@ -16,17 +16,101 @@ jQuery('#custom-owl').owlCarousel({
     }
 });
 
-//   jQuery( document ).ready(function() {
-//       jQuery(".custom-submit-btn").click(function(){
-//		   jQuery(this).css("background-color","#f2f2f2");
-//		   jQuery(this).css("cursor","no-drop");
-////    	jQuery("#form-step-2").css("display","flex");
-//		   jQuery("#form-step-2").css("opacity","1");
-//		   jQuery("#form-step-2").css("transform","scale(1)");
-//		   jQuery("#form-step-2").css("transition","all 0.3s ease-in-out");
-//		   
-//  });
-//    });
+jQuery(document).ready(function($) {
+  var bigimage = $("#big");
+  var thumbs = $("#thumbs");
+  //var totalslides = 10;
+  var syncedSecondary = true;
+
+  bigimage
+    .owlCarousel({
+    items: 1,
+    slideSpeed: 2000,
+    nav: false,
+    autoplay: true,
+    dots: false,
+    loop: true,
+    responsiveRefreshRate: 200,
+    navText: [
+      '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+      '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+    ]
+  })
+    .on("changed.owl.carousel", syncPosition);
+
+  thumbs
+    .on("initialized.owl.carousel", function() {
+    thumbs
+      .find(".owl-item")
+      .eq(0)
+      .addClass("current");
+  })
+    .owlCarousel({
+    items: 4,
+    dots: false,
+    nav: false,
+    navText: [
+      '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+      '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+    ],
+    smartSpeed: 200,
+    slideSpeed: 500,
+    slideBy: 4,
+    responsiveRefreshRate: 100
+  })
+    .on("changed.owl.carousel", syncPosition2);
+
+  function syncPosition(el) {
+    //if loop is set to false, then you have to uncomment the next line
+    //var current = el.item.index;
+
+    //to disable loop, comment this block
+    var count = el.item.count - 1;
+    var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+
+    if (current < 0) {
+      current = count;
+    }
+    if (current > count) {
+      current = 0;
+    }
+    //to this
+    thumbs
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = thumbs.find(".owl-item.active").length - 1;
+    var start = thumbs
+    .find(".owl-item.active")
+    .first()
+    .index();
+    var end = thumbs
+    .find(".owl-item.active")
+    .last()
+    .index();
+
+    if (current > end) {
+      thumbs.data("owl.carousel").to(current, 100, true);
+    }
+    if (current < start) {
+      thumbs.data("owl.carousel").to(current - onscreen, 100, true);
+    }
+  }
+
+  function syncPosition2(el) {
+    if (syncedSecondary) {
+      var number = el.item.index;
+      bigimage.data("owl.carousel").to(number, 100, true);
+    }
+  }
+
+  thumbs.on("click", ".owl-item", function(e) {
+    e.preventDefault();
+    var number = $(this).index();
+    bigimage.data("owl.carousel").to(number, 300, true);
+  });
+});
 
 jQuery(".custom-submit-btn").click(function(){
   	jQuery("#form-step-2").toggleClass("extraFormActive");
@@ -38,6 +122,70 @@ jQuery('#inputDate').datepicker({
 
 
 
+jQuery(".custom-select").each(function() {
+  var classes = jQuery(this).attr("class"),
+      id      = jQuery(this).attr("id"),
+      name    = jQuery(this).attr("name");
+  var template =  '<div class="' + classes + '">';
+      template += '<span class="custom-select-trigger">' + jQuery(this).attr("placeholder") + '</span>';
+      template += '<div class="custom-options">';
+      jQuery(this).find("option").each(function() {
+        template += '<span class="custom-option ' + jQuery(this).attr("class") + '" data-value="' + jQuery(this).attr("value") + '">' + jQuery(this).html() + '</span>';
+      });
+  template += '</div></div>';
+  
+  jQuery(this).wrap('<div class="custom-select-wrapper"></div>');
+  jQuery(this).hide();
+  jQuery(this).after(template);
+});
+jQuery(".custom-option:first-of-type").hover(function() {
+  jQuery(this).parents(".custom-options").addClass("option-hover");
+}, function() {
+  jQuery(this).parents(".custom-options").removeClass("option-hover");
+});
+jQuery(".custom-select-trigger").on("click", function() {
+  jQuery('html').one('click',function() {
+    jQuery(".custom-select").removeClass("opened");
+  });
+  jQuery(this).parents(".custom-select").toggleClass("opened");
+  event.stopPropagation();
+});
+jQuery(".custom-option").on("click", function() {
+  jQuery(this).parents(".custom-select-wrapper").find("select").val(jQuery(this).data("value"));
+  jQuery(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+  jQuery(this).addClass("selection");
+  jQuery(this).parents(".custom-select").removeClass("opened");
+  jQuery(this).parents(".custom-select").find(".custom-select-trigger").text(jQuery(this).text());
+});
+
+
+jQuery('.owl-latestNews').owlCarousel({
+    loop:true,
+    margin:30,
+    dots: false,
+    nav: true,
+    navText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+jQuery('.next-page').click(function(){
+jQuery('.pagination-box ul').find('.pagenumber.active').next().addClass('active');
+jQuery('.pagination-box ul').find('.pagenumber.active').prev().removeClass('active');
+});
+jQuery('.prev-page').click(function(){
+jQuery('.pagination-box ul').find('.pagenumber.active').prev().addClass('active');
+jQuery('.pagination-box ul').find('.pagenumber.active').next().removeClass('active');
+});
 
 const guestBtn = document.querySelector("#guests-input-btn"),
 	guestOptions = document.querySelector("#guests-input-options"),
